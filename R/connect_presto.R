29a9c855d006c7f3105aec52af75c2046d24a704
yaolin <yaolin.bnu@gmail.com>
2018-08-12 18:30:32 +0800

    update connect_presto

---
+++ R/connect_presto.R
@@ -0,0 +1,47 @@
+####################################################################
+#' Connect to server using RPresto
+#'
+#'
+#'
+#' @param server_type string
+#' @param db_type string
+#' @param schema_name string
+#' @export
+
+
+connect_presto <- function(server_type = "nm",
+                           db_type = "dbi",
+                           schema_name = NA_character_) {
+
+  c <- config::get(config = 'presto', file = '~/config.yml')
+  hostname <- sprintf("http://%s",
+                      ifelse(server_type == "nm", c$host_nm,
+                             c$host_md))
+  schemaname <- ifelse(is.na(schema_name), c$schema, schema_name)
+
+  if (db_type == "dbi") {
+    DBI::dbConnect(
+      drv = RPresto::Presto(),
+      host = hostname,
+      port = c$port,
+      user = c$user,
+      password = c$password,
+      schema = schemaname,
+      catalog = c$catalog
+    )
+  } else {
+    pool::dbPool(
+      drv = RPresto::Presto(),
+      host = hostname,
+      port = c$port,
+      user = c$user,
+      password = c$password,
+      schema = schemaname,
+      catalog = c$catalog
+    )
+  }
+
+}
+
+# con <- connect_presto()
+# DBI::dbGetQuery(con, 'select 787')
---
+++ R/update.R
@@ -0,0 +1,30 @@
+####################################################################
+#' Update the library
+#' (borrowed from laresbernardo/lares)
+#' This function lets the user update from repository or local source.
+#'
+#' @param local Boolean. Install package with local files (TRUE) or Github repository
+#' @param force Boolean. Force install if needed
+#' @param restart Boolean. Restart session after re-installing the library
+#' @export
+
+
+update_RYLin <- function(local = FALSE, force = FALSE, restart = FALSE) {
+
+  RYLin::load_packages(c('devtools', 'config'))
+  # suppressMessages(require(devtools))
+  # suppressMessages(require(config))
+
+  start <- Sys.time()
+  message(paste(start,"| Started installation..."))
+
+  if (local == TRUE) {
+    devtools::install("~/RYLin/")
+  } else {
+    devtools::install_github("iyaolin/RYLin", force = force)
+  }
+  if (restart == TRUE) {
+    .rs.restartR()
+  }
+  message(paste(Sys.time(), "| Duration:", round(difftime(Sys.time(), start, units = "secs"), 2), "s"))
+}
